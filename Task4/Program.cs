df76699c2022e2e96d6805289b525a39f7d791b4
Dmitriy V <faustt@bk.ru>
2022-07-22 19:22:40 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 0;
+
+Console.Write("Введите число: ");
+n = int.Parse(Console.ReadLine());
+
+int index = 1;
+
+while (index <= n)
+{
+    if(index % 2 == 0)
+    {
+        Console.Write($"{index},");
+    }
+    index++;
+}
