1daf035e498904584c1fb16fefbe904a21342eee
Dmitriy V <faustt@bk.ru>
2022-07-22 15:46:19 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int max = numberA;
+if (numberB > numberA) 
+{
+    Console.WriteLine("max = numberB");
+}
+else 
+{
+    Console.WriteLine("max = numberA");
+}
