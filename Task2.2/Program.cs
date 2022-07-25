54063ff383444d9bc017fa87de9b17dde51324a0
Dmitriy V <faustt@bk.ru>
2022-07-25 11:00:05 +0300

    добавил вторую задачу по 2-му семинару

---
+++ Task2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 0;
+Console.Write("Введите число: ");
+n = int.Parse(Console.ReadLine());
+
+if (n>99)
+{
+    Console.WriteLine(n.ToString()[2]);
+}
+else 
+{
+    Console.WriteLine("третьей цифры нет");
+}
