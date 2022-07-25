824435aefa29fc882d37cd75e217168d16834784
Dmitriy V <faustt@bk.ru>
2022-07-25 10:42:05 +0300

    первая задача после 2-го семинара

---
+++ Task2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число: \nДля выхода из программы введите команду Stop. ");
+
+while(true)
+    {
+        string userEnter = Console.ReadLine();
+        if(!userEnter.Equals("Stop"))
+        {
+            Console.WriteLine("{0}->{1}", userEnter, userEnter[1]);
+        }
+        else
+            break;
+    }
