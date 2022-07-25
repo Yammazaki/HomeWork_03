d7b541e055bfdb60e341e7143b572d310fb97e4e
Dmitriy V <faustt@bk.ru>
2022-07-25 11:07:52 +0300

    задача с днями недели по 2-му семинару

---
+++ Task2.3/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = 0;
+Console.Write("Введите номер недели: ");
+string userEnter = Console.ReadLine();
+number = int.Parse(userEnter);
+
+if (number > 5)
+{
+    Console.Write("Yes");
+}
+else 
+    Console.Write("No");
