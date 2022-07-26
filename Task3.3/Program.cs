cb0ed49d1910732283314b09a1eb30e3e24a9d2f
Dmitriy V <faustt@bk.ru>
2022-07-26 20:17:29 +0300

    задача на кубы

---
+++ Task3.3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int n = 0;
+
+for (int i = 1; i <= a; i++)
+{
+    n = i*i*i;
+    Console.WriteLine(n);
+}
