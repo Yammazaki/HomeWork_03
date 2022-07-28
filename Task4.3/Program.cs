6551768f446bf3d96c614d8e3581e20e7c8a2b77
Dmitriy V <faustt@bk.ru>
2022-07-28 17:33:00 +0300

    Задача 29, но не уверен, что это рям то самое

---
+++ Task4.3/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+void FillArray (int [] cool)
+{
+    int b = cool.Length;
+    int i1 = 0;
+    while(i1 < b)
+    {
+        cool[i1] = new Random().Next(1, 10);
+        i1++;
+    }
+}
+void PrintArray (int[] col)
+{
+    int n = col.Length;
+    int i = 0;
+    while (i < n)
+    {
+        Console.Write($"{col[i]}, ");
+        i++;
+    }
+}
+int[] array = new int [8];
+FillArray(array);
+PrintArray(array);
