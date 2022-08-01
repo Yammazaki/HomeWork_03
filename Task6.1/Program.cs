24623ff1cd05ac59d63797ef6ce90622f596e08c
Dmitriy V <faustt@bk.ru>
2022-08-01 15:38:12 +0300

    подсчет положительных чисел

---
+++ Task6.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество чисел М: ");
+int size = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for(int i = 0; i < size; i++)
+{
+    Console.Write("Введите число: ");
+    int num = int.Parse(Console.ReadLine());
+    if(num > 0)
+    {
+        sum ++;
+    }
+}
+Console.WriteLine($"Количество положительных чисел равно {sum} ");
