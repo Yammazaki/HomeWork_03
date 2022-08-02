874ccf6b07730c6a144a50f735bc1a3f2279c048
Dmitriy V <faustt@bk.ru>
2022-08-02 19:51:03 +0300

    Начал задачу, но еще не все

---
+++ Task7.2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int[,] array = new int [5, 5];
+
+for (int i =0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        array[i,  j] = new Random().Next(10); 
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i =0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+int Find (int[,] array)
+{
+    int find = int.Parse(Console.ReadLine());
+    for (int i = 0; i < 5; i++)
+    {
+        for( int j = 0; j < 5; j++)
+        {
+            if(array[i,j] == find)
+        {
+            Console.WriteLine($"{array[i,j]} ");
+            break;
+        }
+        }
+ 
+    }
+    return array;
+}
+
+PrintArray(array); 
+Console.WriteLine(Find(array));
