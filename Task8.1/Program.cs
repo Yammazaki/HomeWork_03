84da071788c6a5232e366fd43a4fc47ee4eec241
Dmitry V <faustt@bk.ru>
2022-08-04 15:49:11 +0300

    перебор массива

---
+++ Task8.1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int m = 0;
+int n = 0;
+int.TryParse(Console.ReadLine(), out m);
+int.TryParse(Console.ReadLine(), out n);
+
+int[,] array = new int[m, n];
+
+for (int i =0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i,  j] = new Random().Next(10); 
+        Console.Write($"{array[i, j]} " );
+    }
+    Console.WriteLine();
+}
+for(int k = 0; k < n; k++)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int min = 0;
+            min = array[j,i];
+            if(array[j,i] <= array[j,i + 1])
+            {
+                array[j,i] = array[j,i + 1];
+                array[j,i + 1] = min;
+            }
+        }
+    }
+}
+Console.WriteLine();
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.Write($"\n");
+} 
