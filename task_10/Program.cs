ca3830aa9ec4c42180f2bea3ad88f3aaac498c1f
Dmitry V <faustt@bk.ru>
2022-08-04 22:23:41 +0300

    seminar 9

---
+++ task_10/Program.cs
@@ -0,0 +1,28 @@
+﻿/* int N = 0;
+int.TryParse(Console.ReadLine(), out N);
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end)
+        return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+
+Console.Write(PrintNumbers(1, N));  
+
+// поменяли с м до н
+int N = 0;
+int M = 0;
+int.TryParse(Console.ReadLine(), out N);
+int.TryParse(Console.ReadLine(), out M);
+
+string PrintNumbers(int start, int end)
+{   
+     if (start == end)
+        return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+
+
+Console.Write(PrintNumbers(N, M)); */
+
