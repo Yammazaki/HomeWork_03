5d755d384e941fa5241050fca1fd92524f9e11df
Dmitry V <faustt@bk.ru>
2022-08-05 15:55:30 +0300

    программа, которая выводит все натуральные числа в промежутке от M до N.

---
+++ Task9.1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+int N = 0;
+int M = 0;
+int.TryParse(Console.ReadLine(), out N);
+int.TryParse(Console.ReadLine(), out M);
+
+string PrintNumbers(int start, int end)
+{   
+     if (start == end)
+        return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+
+
+Console.Write(PrintNumbers(N, M));
