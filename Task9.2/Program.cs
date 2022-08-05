477df81062f456ea91854534f065412f860c4db3
Dmitry V <faustt@bk.ru>
2022-08-05 17:10:13 +0300

    программа, которая выводит сумму всех натуральных чисел от м до н

---
+++ Task9.2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int N = 0;
+int M = 0;
+int.TryParse(Console.ReadLine(), out N);
+int.TryParse(Console.ReadLine(), out M);
+int sum = 0; 
+
+for(int i = N; i <= M; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"{sum} ");
